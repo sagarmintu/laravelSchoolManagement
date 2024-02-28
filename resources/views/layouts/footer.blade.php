7549c8bf8e4e8d713808d66b05aaa9c727ba821f
sagarmintu <sagarskbehera1996@gmail.com>
2024-02-28 16:23:30 +0530

    setup admin dashboard and layouts of it

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024-2025 <a href="#">Ralecon</a>.</strong> All rights reserved.
+</footer>
+
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>

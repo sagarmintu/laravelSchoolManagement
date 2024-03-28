82cf350d60a8e76a9952c823a05ee4f8a9987b21
sagarmintu <sagarskbehera1996@gmail.com>
2024-03-28 11:41:34 +0530

    show result of the exam

--- resources/views/student/exam_result.blade.php
+++ resources/views/student/exam_result.blade.php
@@ -29,7 +29,7 @@
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
-                                            <th>Subject Name</th>
+                                            <th>Subject</th>
                                             <th>Class Work</th>
                                             <th>Test Work</th>
                                             <th>Home Work</th>
@@ -37,12 +37,25 @@
                                             <th>Total Score</th>
                                             <th>Pass Work</th>
                                             <th>Full Mark</th>
+                                            <th>Result</th>
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        @php
+                                            $total_score = 0;
+                                            $full_mark = 0;
+                                            $result_validate = 0;
+                                        @endphp
+
                                         @foreach($value['subject'] as $exam)
+
+                                        @php
+                                            $total_score = $total_score + $exam['total_score'];
+                                            $full_mark = $full_mark + $exam['full_mark'];
+                                        @endphp
+
                                             <tr>
-                                                <td>{{ $exam['subject_name'] }}</td>
+                                                <td style="width: 300px;">{{ $exam['subject_name'] }}</td>
                                                 <td>{{ $exam['class_work'] }}</td>
                                                 <td>{{ $exam['home_work'] }}</td>
                                                 <td>{{ $exam['test_work'] }}</td>
@@ -50,8 +63,35 @@
                                                 <td>{{ $exam['total_score'] }}</td>
                                                 <td>{{ $exam['pass_mark'] }}</td>
                                                 <td>{{ $exam['full_mark'] }}</td>
+                                                <td>
+                                                    @if($exam['total_score'] >= $exam['pass_mark'])
+                                                        <span style="color: green; font-weight: bold;">Pass</span>
+                                                    @else
+                                                        @php
+                                                            $result_validate = 1;
+                                                        @endphp
+                                                        <span style="color: red; font-weight: bold;">Fail</span>
+                                                    @endif
+                                                </td>
                                             </tr>
                                         @endforeach
+                                            <tr>
+                                                <td colspan="2">
+                                                    <b>Grand Total : {{ $total_score }} / {{ $full_mark }}</b>
+                                                </td>
+                                                <td colspan="3">
+                                                    <b>Percentage : {{ round(($total_score * 100) / $full_mark,2) }}%</b>
+                                                </td>
+                                                <td colspan="5">
+                                                    <b>Result : 
+                                                        @if($result_validate == 0) 
+                                                            <span style="color: green; font-weight: bold;">Pass</span> 
+                                                        @else 
+                                                            <span style="color: red; font-weight: bold;">Fail</span>
+                                                        @endif
+                                                    </b>
+                                                </td>
+                                            </tr>
                                     </tbody>
                                 </table>
 
